7feaa66d407e42eac680091529ce5c3b88e1992d
toshetasevski <toshetasevski@Toshes-MacBook-Pro.local>
2022-05-17 09:57:58 -0400

    users list component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AddUser from './User/User';
+import UserList from './User/UserList';
 
 
 function App() {
   return (
     <div>
         <AddUser />
+        <UserList users={[]} />
     </div>
   );
 }
